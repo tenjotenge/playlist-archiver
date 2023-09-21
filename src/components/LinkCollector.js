7b816fdcf76791a5b3973682e6f0c5bd35527cfc
nayatrov <nayatrov@outlook.com>
2023-09-21 15:09:33 -0400

    now includes downloadbutton and does not expect content response from endpoint

--- src/components/LinkCollector.js
+++ src/components/LinkCollector.js
@@ -1,40 +1,35 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import DownloadButton from './DownloadButton'; // Import your DownloadButton component
 
 function LinkCollector() {
   const [playlistURL, setPlaylistURL] = useState('');
-  const [message, setMessage] = useState('');
-  
-  const handleDownload = async () => {
-    try {
-      const response = await axios.post('/api/archive-spotify-playlist-api-endpoint', {
-        playlistURL,
-      });
-
-      const blob = new Blob([JSON.stringify(response.data)], {
-        type: 'application/json',
-      });
-      
-      const url = window.URL.createObjectURL(blob);
-      const a = document.createElement('a');
-      a.href = url;
-      a.download = `${playlistName}-playlist.json`; // Specify the desired file name
-      document.body.appendChild(a);
-      a.click();
-      window.URL.revokeObjectURL(url);
-    } catch (error) {
-      console.error('Error archiving playlist:', error);
-    }
-  };
 
   const handleChange = (event) => {
     setPlaylistURL(event.target.value);
+  };
+  const playlistName = ''
+
+  // Modify this function to trigger the API call when the user clicks the button
+  const handleDownload = () => {
+    axios
+      .post('/api/archive-spotify-playlist-api-endpoint', {
+        playlistURL,
+      })
+      .then(() => {
+        // Handle success if needed
+        console.log('Playlist archived successfully');
+      })
+      .catch((error) => {
+        console.error('Error archiving playlist:', error);
+      });
   };
 
   return (
     <div>
       <input type="text" placeholder="Enter Spotify Playlist Link" onChange={handleChange} />
-      <button onClick={handleDownload}>Download Playlist</button>
+      <button onClick={handleDownload}>Archive Playlist</button>
+      <DownloadButton fileName={`${playlistName}-playlist.json`} />
     </div>
   );
 }
