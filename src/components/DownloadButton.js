8f5778818c9eaa05839921ce97a07406563a5e77
nayatrov <nayatrov@outlook.com>
2023-10-15 00:45:07 -0400

    maybe

--- src/components/DownloadButton.js
+++ src/components/DownloadButton.js
@@ -4,13 +4,13 @@ import styles from './DownloadButton.module.css';
 
 const cx = classNames.bind(styles);
 
-function DownloadButton({ fileName }) {
+function DownloadButton() {
   const downloadUrl = '/public/downloads/playlist.json';
 
   const handleDownload = () => {
     const link = document.createElement('a');
     link.href = downloadUrl;
-    link.download = fileName;
+    link.download = 'playlist.json';
     link.click();
   };
 
