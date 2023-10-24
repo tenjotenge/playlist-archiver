beb99763ad7d709eba5abd92b9a02f0ccd8fc9b2
nayatrov <nayatrov@outlook.com>
2023-10-24 14:48:20 -0400

    yes

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import LinkCollector from '../src/components/LinkCollector';
 import Banner from '../src/components/Banner';
+import LinkBar from '@/components/LinkBar';
 
 function Home() {
   return (
     <div>
       <Banner />
       <LinkCollector />
+      <LinkBar />
     </div>
   );
 }
