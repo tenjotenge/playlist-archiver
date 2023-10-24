31adf062914d33e835405eb22e1e6bec04888b68
nayatrov <nayatrov@outlook.com>
2023-10-24 14:06:07 -0400

    start of linkbar

---
+++ src/components/LinkBar.js
@@ -0,0 +1,24 @@
+// LinkBar.js
+import React from 'react';
+import classNames from 'classnames/bind';
+import styles from './LinkBar.module.css';
+
+const cx = classNames.bind(styles);
+
+const LinkBar = () => {
+  return (
+    <div className={cx('lb-container')}>
+      <a href="https://github.com/yourgithubusername" target="_blank" rel="noopener noreferrer">
+        <img src="github.png" alt="GitHub" />
+      </a>
+      <a href="https://yourportfoliowebsite.com" target="_blank" rel="noopener noreferrer">
+        <img src="portfolio.png" alt="Portfolio" />
+      </a>
+      <button onClick={() => console.log('Third button clicked')}>
+        <img src="third.png" alt="Third" />
+      </button>
+    </div>
+  );
+};
+
+export default LinkBar;
