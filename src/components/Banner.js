c585b330727dee52a6039522e819fca086296cc8
nayatrov <nayatrov@outlook.com>
2023-11-03 10:30:15 -0400

    more south beach and favicon

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,11 +4,13 @@ import styles from './Banner.module.css';
 
 const cx = classNames.bind(styles);
 
+// banner text element was removed but keeping it below just in case
+// <p className={cx('banner-text')}>Enter a Spotify playlist link to archive it.</p>
+
 const Banner = () => {
   return (
     <div className={cx('banner-container')}>
       <h1 className={cx('banner-heading')}>Welcome to The Playlist Archiver</h1>
-      <p className={cx('banner-text')}>Enter a Spotify playlist link to archive it.</p>
     </div>
   );
 };
