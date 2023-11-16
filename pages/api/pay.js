66e3d780ab454af4556c0156a94ff576835ece99
nayatrov <nayatrov@outlook.com>
2023-11-15 23:05:12 -0500

    testing

---
+++ pages/api/pay.js
@@ -0,0 +1,37 @@
+import { Client } from 'square';
+import { randomUUID } from 'crypto';
+
+require('dotenv').config();
+
+// Define a custom toJSON method for BigInt to prevent serialization error
+BigInt.prototype.toJSON = function () {
+  return this.toString();
+};
+
+const { paymentsApi } = new Client({
+  accessToken: process.env.SQUARE_ACCESS_TOKEN,
+  environment: 'sandbox'
+});
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    try {
+      const { result } = await paymentsApi.createPayment({
+        idempotencyKey: randomUUID(),
+        sourceId: req.body.sourceId,
+        amountMoney: {
+          currency: 'USD',
+          amount: 100
+        }
+      });
+
+      console.log(result);
+      res.status(200).json(result);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Error processing payment');
+    }
+  } else {
+    res.status(500).send('Invalid request method');
+  }
+}
--- src/components/CreditCardForm.js
+++ src/components/CreditCardForm.js
@@ -7,17 +7,37 @@ import { CreditCard } from 'react-square-web-payments-sdk';
 const cx = classNames.bind(styles);
 
 const CreditCardForm = () => {
+  const handleCardTokenizeResponse = async (token, verifiedBuyer) => {
+    try {
+      const response = await fetch("/api/pay", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({
+          sourceId: token.token,
+        }),
+      });
+
+      const responseData = await response.json();
+      console.log(responseData);
+
+      // Handle the response as needed (e.g., show success message to the user)
+    } catch (error) {
+      console.error('Error processing payment:', error);
+
+      // Handle the error as needed (e.g., show error message to the user)
+    }
+  };
+
   return (
     <div className={cx('payment-container')}>
       <PaymentForm
         applicationId="sandbox-sq0idb-cuAr6GdfGcn6miY7Y7JN_Q"
-        cardTokenizeResponseReceived={(token, verifiedBuyer) => {
-          console.log('token:', token);
-          console.log('verifiedBuyer:', verifiedBuyer);
-        }}
+        cardTokenizeResponseReceived={handleCardTokenizeResponse}
         locationId='XXXXXXXXXX'
       >
-        <CreditCard/>
+        <CreditCard />
       </PaymentForm>
     </div>
   );
